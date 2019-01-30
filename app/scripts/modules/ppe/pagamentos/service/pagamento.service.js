32075fcbd46f37097a762fc7dd811bada049909f
brunocarneiro <bruno.carneiro312@gmail.com>
2019-01-30 12:34:41 -0200

    setup ppe

--- app/scripts/modules/ppe/pagamentos/service/pagamento.service.js
+++ app/scripts/modules/ppe/pagamentos/service/pagamento.service.js
@@ -6,6 +6,42 @@ module.exports = function(module) {
 
     function PagamentoService() {
 
+        return {
+            salvar:  salvar,
+            remover: remover,
+            listar:  listar,
+            buscar:  buscar
+        }
 
+        /**
+         * Solicita recurso para salvar pagamento
+         */
+        function salvar(pagamento) {
+            console.log("salvando pagamento...");
+            console.log(pagamento);
+        }
+
+        /**
+         * Solicita recurso para remover pagamento
+         */
+        function remover(pagamento) {
+            console.log("removendo pagamento...");
+            console.log(pagamento)
+        }
+
+        /**
+         * Solicita recurso para listar pagamento
+         */
+        function listar() {
+            console.log("listando pagamento...");
+        }
+
+        /**
+         * Solicita recurso para buscar pagamento
+         */
+        function buscar(codigoDoPagamento) {
+            console.log("buscando pagamento...");
+            console.log(codigoDoPagamento);
+        }
     }
 }
